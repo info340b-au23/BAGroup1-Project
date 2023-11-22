0488e0b9543cfdc16a2b0ba9cb0e3772efdd68f7
AdamSkog <adamskoglund2022@gmail.com>
2023-11-21 20:28:27 -0800

    Completed DecksView page

--- studyspark/src/components/DeckCard.js
+++ studyspark/src/components/DeckCard.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function DeckCard({ title, description }) {
+export default function DeckCard({
+	title,
+	description,
+	showForm,
+	handleRemoveDeck,
+}) {
 	return (
 		<div className="col-md-6 col-xl-3 d-flex">
-			<Link to={`/deck-${title}`}>
-				<div className="card mb-4 deck">
-					<div className="card-body deck-body">
-						<div className="row">
-							<div className="col-sm-auto col-xl-12"></div>
-							<div className="col-sm">
+			<div className="card mb-4 deck">
+				<div className="card-body deck-body">
+					<div className="row">
+						<div className="col-sm">
+							<Link to={`/deck-${title}`}>
 								<h2 className="card-title">{title}</h2>
-								<p className="card-text">{description}</p>
-							</div>
+							</Link>
+							<p className="card-text">{description}</p>
 						</div>
+						{showForm && (
+							<div className="col-2">
+								<button
+									className="btn btn-sm btn-danger"
+									onClick={handleRemoveDeck}
+								>
+									X
+								</button>
+							</div>
+						)}
 					</div>
 				</div>
-			</Link>
+			</div>
 		</div>
 	);
 }
--- studyspark/src/components/DecksView.js
+++ studyspark/src/components/DecksView.js
@@ -1,50 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import DeckCard from "./DeckCard";
 
 export default function Decks(props) {
-	const deckCards = props.decks.map((deck, index) => {
+	const [showForm, setShowForm] = useState(false);
+	const [decks, setDecks] = useState(props.decks);
+
+	function handleEditDecks() {
+		setShowForm(!showForm);
+	}
+
+	const deckCards = decks.map((deck, index) => {
+		function handleRemoveDeck() {
+			const updatedDecks = [...decks];
+			updatedDecks.splice(index, 1);
+			setDecks(updatedDecks);
+		}
+
 		return (
 			<DeckCard
 				key={index}
 				title={deck.title}
 				description={deck.description}
+				showForm={showForm}
+				handleRemoveDeck={handleRemoveDeck}
 			/>
 		);
 	});
+
 	return (
 		<main className="container d-flex flex-column text-center flex-grow-1 mt-5">
 			<div>
-				<h1 className="display-3 fw-bold my-5">Your Decks</h1>
+				<h1 className="display-3 fw-bold mt-5">Your Decks</h1>
 			</div>
+			<button
+				className="btn btn-primary mb-4 w-25 mx-auto justify-content-center"
+				onClick={handleEditDecks}
+			>
+				{showForm ? "Cancel" : "Edit Decks"}
+			</button>
 			<div className="container">
 				<div className="row">
+					{showForm ? (
+						<CreateDeckForm decks={decks} setDecks={setDecks} />
+					) : null}
 					{deckCards}
-					<div className="col-md-6 col-xl-3 d-flex">
-						<div className="card mb-4 deck">
-							<div className="card-body deck-body">
-								<div className="row">
-									<div className="col-sm-auto col-xl-12"></div>
-									<div className="col-sm">
-										<h2 className="card-title">
-											Create New...
-										</h2>
-										<p className="card-text">
-											Maiores voluptate rem odit dolor
-											temporibus asperiores maxime velit
-											sapiente.
-										</p>
-										<a
-											href="deck.html"
-											// THIS WILL CHANGE
-											className="stretched-link"
-										></a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</main>
+	);
+}
+
+function CreateDeckForm({ decks, setDecks }) {
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+
+	function handleSubmit(e) {
+		e.preventDefault(); // prevent default form submission which would refresh the page
+		const newDeck = { title, description };
+		const updatedDecks = [...decks, newDeck];
+		setDecks(updatedDecks);
+		// The following is for adding to noSQL database once we include firebase functionality!
+		// fetch("../data/decks.json", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json",
+		// 	},
+		// 	body: JSON.stringify(newDeck),
+		// })
+		// 	.then((response) => {
+		// 		console.log(response);
+		// 		return response.json();
+		// 	})
+		// 	.catch((error) => {
+		// 		console.error("Error:", error);
+		// 	});
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className="p-3 bg-dark rounded-4 mb-4">
+			<div className="mb-3">
+				<label htmlFor="title" className="form-label">
+					Title:
+				</label>
+				<input
+					type="text"
+					id="title"
+					className="form-control"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+			</div>
+			<div className="mb-3">
+				<label htmlFor="description" className="form-label">
+					Description:
+				</label>
+				<input
+					type="text"
+					id="description"
+					className="form-control"
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+				/>
+			</div>
+			<button type="submit" className="btn btn-primary">
+				Submit
+			</button>
+		</form>
 	);
 }
