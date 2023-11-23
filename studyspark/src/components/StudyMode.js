e7ea38a022a75901469f88f276a291331eb87cbc
Shelly Zhao <qingyz23@uw.edu>
2023-11-22 21:20:45 -0800

    StudyMode static

---
+++ studyspark/src/components/StudyMode.js
@@ -0,0 +1,43 @@
+import React from "react";
+import CardsView from "./CardsView";
+
+
+export default function StudyMode() {
+    return (
+        <body class="d-flex flex-column vh-100">
+            <time id="timer" class="timer-container">
+                <span id="minutes">00</span>:<span id="seconds">00</span>
+            </time>
+
+            <main class="study-set">
+                <h1 class="deck-title">Deck Title/Progress</h1>
+
+                <div class="flashcard" onclick="toggleFlip()">
+                    <div class="flashcard-inner">
+                        <div class="flashcard-front">Front Content</div>
+                        <div class="flashcard-back">
+                            Back Content
+                            <button class="progress-button incorrect-button">
+                                <span class="arrow">&#8678;</span>
+                                Wrong
+                            </button>
+                            <button class="progress-button correct-button">
+                                Correct
+                                <span class="arrow">&#8680;</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <script>
+                    function toggleFlip() {
+                        var flashcard = document.querySelector(".flashcard");
+                        flashcard.classList.toggle("flipped");
+                    }
+                </script>
+
+                <button class="shuffle-button">Shuffle</button>
+            </main>
+        </body>
+    );
+}
