9c4ec1ce2c7d1434796abc328ea96578e78eacde
AdamSkog <adamskoglund2022@gmail.com>
2023-12-10 23:14:21 -0800

    cleaned up index.js firebase initialization

--- studyspark/src/index.js
+++ studyspark/src/index.js
@@ -5,7 +5,6 @@ import { initializeApp } from "firebase/app";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.css";
 import "./style.css";
-import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
 	apiKey: "AIzaSyDMJJ_WEPBr3N-d8za6wmzH-Je04E6QJdQ",
@@ -19,18 +18,6 @@ const firebaseConfig = {
   
 
 initializeApp(firebaseConfig);
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDMJJ_WEPBr3N-d8za6wmzH-Je04E6QJdQ",
-  authDomain: "studyspark-97b92.firebaseapp.com",
-  databaseURL: "https://studyspark-97b92-default-rtdb.firebaseio.com",
-  projectId: "studyspark-97b92",
-  storageBucket: "studyspark-97b92.appspot.com",
-  messagingSenderId: "337626103078",
-  appId: "1:337626103078:web:f6504517c82ed9843355e7"
-};
-
-const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
