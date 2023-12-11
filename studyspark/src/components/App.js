5b0b71d841a5cc087b19cbb9db6e0ff7726d4a24
Troy Lu <troylu6@gmail.com>
2023-12-11 00:46:14 -0800

    added firebaseUI login and signup functionality, reworked navbar

--- studyspark/src/components/App.js
+++ studyspark/src/components/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Routes, Route, useLocation, Navigate, Outlet } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useLocation, Navigate, useNavigate, Outlet } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Navbar from "./Navbar";
 import HomePage from "./HomePage";
 import Decks from "./DecksView";
@@ -12,15 +13,37 @@ import StudyMode from "./StudyMode";
 export default function App(props) {
   const [currentUser, setCurrentUser] = useState(null); // ! Need to set user functionality
   const location = useLocation();
-
   const isHomePage = location.pathname === "/";
+  const navigateTo = useNavigate();
+  
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        user.userName = user.displayName;
+        user.userId = user.uid;
+        setCurrentUser(user);
+      }
+      else {
+        setCurrentUser(null);
+      }
+    })
+  }, [])
+
+  const loginUser = (userObj) => {
+    setCurrentUser(userObj);
+    
+    if(userObj.userId !== null){
+      navigateTo('/decks'); 
+    }
+  }
 
   return (
     <div className={`d-flex flex-column vh-100 ${isHomePage ? "title" : ""}`}>
-      <Navbar />
+      <Navbar currentUser={currentUser}/>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="login" element={<Login />} />
+        <Route path="login" element={<Login currentUser={currentUser} loginCallback={loginUser}/>} />
         <Route path="signup" element={<Signup />} />
         {/* Protected Routes */}
         <Route element={<ProtectedPage currentUser={currentUser} />}>
@@ -35,7 +58,7 @@ export default function App(props) {
 
 function ProtectedPage(props) {
   if (props.currentUser === null) { // TODO change to .userId or whatever we use for users
-    return <Navigate to="/signup" />;
+    return <Navigate to="/login" />;
   } else {
     return <Outlet />;
   }
